9ff726cee137e8e983a5209631d1a64f79570f8c
xun changqing <xunchangqing@qq.com>
2024-08-21 08:21:45 +0800

    WORK: 1. 增加 emu，sim 的 ivy 配置

---
+++ ivy_cfg/emu.py
@@ -0,0 +1,4 @@
+import ivy.cfg
+
+ivy_cfg = ivy.cfg.Config()
+ivy_cfg.no_booter = False
---
+++ ivy_cfg/sim.py
@@ -0,0 +1,5 @@
+import ivy.cfg
+
+ivy_cfg = ivy.cfg.Config()
+ivy_cfg.no_booter = True
+# ivy_cfg.load_addr = 0x80080000
