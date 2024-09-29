9654ecfa9e404a92c718dbcf8305a21e142ec4ff
xun changqing <xunchangqing@qq.com>
2024-09-29 08:56:53 +0800

    WORK: 1. memtester 完善

--- memtester/main.c
+++ memtester/main.c
@@ -13,80 +13,47 @@
 #include "types.h"
 #include "xrt.h"
 
-// 每个 numa 域只使用一个处理核进行测试
-#define ONE_CORE_PER_NUMA
-// 处理核只测试相同 numa 域的存储
-#define LOCAL_MEM_ONLY
-
 // 所有cpu依次进行memory test
 volatile int cur_cpu = 0;
 
-bool cpu_done[IVY_DT_NR_CPUS] = {false};
-
 void memory_test() {
   printf("memory test on cpu: %d\n", cur_cpu);
-  uint64_t times = 0;
-  uint64_t pattern_offset = 0;
-  uint64_t pattern = 0;
-  uint64_t s = 0;
-  uint64_t cur_cpu_numa_id;
-  ptrdiff_t pagesizemask;
-  void volatile *buf, *aligned;
-  ulv *bufa, *bufb;
-  size_t pagesize, wantraw, wantmb, wantbytes, wantbytes_orig, bufsize, halflen,
-      count;
+  uint64_t idx = 0;
 
-  cpu_done[cur_cpu] = true;
+  for (int mr = 0; mr < IVY_DT_NUM_FREE_MEMORY; mr++) {
+    printf("setting memory region start: 0x%lx size: 0x%lx\n",
+           ivy_dt_free_memories[mr].start, ivy_dt_free_memories[mr].size);
+    uint64_t mr_start = ivy_dt_free_memories[mr].start;
+    uint64_t mr_size = ivy_dt_free_memories[mr].size;
+    ulv *buf = (ulv *)(mr_start);
 
-  cur_cpu_numa_id = ivy_dt_cpus[cur_cpu].numa_id;
-
-#ifdef ONE_CORE_PER_NUMA
-  for (int ci = 0; ci < IVY_DT_NR_CPUS; ci++) {
-    if (ci != cur_cpu && cpu_done[ci] &&
-        cur_cpu_numa_id == ivy_dt_cpus[ci].numa_id) {
-      printf("skip memory test on cpu: %d due to: %d in the same numa\n",
-             cur_cpu, ci);
-      return;
+    for (size_t mr_j = 0; mr_j < mr_size / sizeof(uint64_t); mr_j++) {
+      *buf = ((idx) % 2) == 0 ? (ul)buf : ~((ul)buf);
+      idx++;
+      buf++;
     }
   }
-#endif
 
-  pagesize = IVY_CFG_PAGE_SIZE;
-  pagesizemask = (ptrdiff_t) ~(pagesize - 1);
+  idx = 0;
 
-  // 所有可用存储段大跨度检查一遍，防止地址高位发生折叠
   for (int mr = 0; mr < IVY_DT_NUM_FREE_MEMORY; mr++) {
-    printf("test memory region start: 0x%lx size: 0x%lx\n",
+    printf("testing memory region start: 0x%lx size: 0x%lx\n",
            ivy_dt_free_memories[mr].start, ivy_dt_free_memories[mr].size);
+    uint64_t mr_start = ivy_dt_free_memories[mr].start;
+    uint64_t mr_size = ivy_dt_free_memories[mr].size;
+    ulv *buf = (ulv *)(mr_start);
 
-#ifdef LOCAL_MEM_ONLY
-    if (cur_cpu_numa_id != ivy_dt_free_memories[mr].numa_id) {
-      printf("skip memory region in a different numa\n");
-      continue;
+    for (size_t mr_j = 0; mr_j < mr_size / sizeof(uint64_t); mr_j++) {
+      uint64_t exp_val = ((idx) % 2) == 0 ? (ul)buf : ~((ul)buf);
+      if (*buf != exp_val) {
+        printf("FAILURE: dut val 0x%08lx exp val 0x%08lx, address 0x%08lx\n",
+               *buf, exp_val, (ul)buf);
+        xrt_exit(1);
+      }
+
+      idx++;
+      buf++;
     }
-#endif
-
-    buf = (ulv *)ivy_dt_free_memories[mr].start;
-    bufsize = ivy_dt_free_memories[mr].size;
-    // Do alighnment here as well, as some cases won't trigger above if you
-    // define out the use of mlock() (cough HP/UX 10 cough).
-    if ((size_t)buf % pagesize) {
-      /* printf("aligning to page -- was 0x%tx\n", buf); */
-      aligned = (void volatile *)((size_t)buf & pagesizemask) + pagesize;
-      /* printf("  now 0x%tx -- lost %d bytes\n", aligned,
-       *      (size_t) aligned - (size_t) buf);
-       */
-      bufsize -= ((size_t)aligned - (size_t)buf);
-    } else {
-      aligned = buf;
-    }
-
-    halflen = bufsize / 2;
-    count = halflen / sizeof(ul);
-    bufa = (ulv *)aligned;
-    bufb = (ulv *)((size_t)aligned + halflen);
-
-    test_stuck_address(aligned, bufsize / sizeof(ul));
   }
 }
 
@@ -109,7 +76,7 @@ void choose_local_cpu() {
   }
 }
 
-void do_memtest(uint64_t start, uint64_t size) {
+void test_range(uint64_t start, uint64_t size) {
   uint64_t times = 0;
   uint64_t pattern_offset = 0;
   uint64_t pattern = 0;
@@ -172,7 +139,7 @@ void para_local_memtest() {
       printf("test memory region 0x%lx 0x%lx on cpu %d\n", fm_start, fm_size,
              this_cpu);
 
-      do_memtest(fm_start, fm_size);
+      test_range(fm_start, fm_size);
     }
   }
 }
@@ -185,6 +152,20 @@ void xmain() {
     choose_local_cpu();
   }
 
+  while (cur_cpu != this_cpu) {
+    WFE();
+  }
+  DSB();
+
+  // 所有处理核依次测试所有存储
+  memory_test();
+
+  DSB();
+  cur_cpu++;
+  // 在发出SEV之前必须 DSB，确保被唤醒的核能够看到 cur_cpu 的新值
+  DSB();
+  SEV();
+
   DSB();
   tmp_barrier += 1;
   DSB();
@@ -194,18 +175,6 @@ void xmain() {
     WFE();
   }
 
+  // 每个 numa 一个处理核大压力测试本 numa 内存储，使用 memtester 内方法
   para_local_memtest();
-
-  // while (cur_cpu != this_cpu) {
-  //   WFE();
-  // }
-  // DSB();
-
-  // memory_test();
-
-  // DSB();
-  // cur_cpu++;
-  // 在发出SEV之前必须 DSB，确保被唤醒的核能够看到 cur_cpu 的新值
-  // DSB();
-  // SEV();
 }
